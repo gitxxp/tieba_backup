3ee6577559499a1c7db6f8a49a1f46e2ab66d213
gitxxp <33143774+gitxxp@users.noreply.github.com>
2019-08-04 17:12:02 +0800

    Update 批量备份帖子.py

--- 批量备份帖子.py
+++ 批量备份帖子.py
@@ -1,34 +1,52 @@
 import requests,re,os
 from bs4 import BeautifulSoup
-import 贴吧只看楼主下载
+import 贴吧只看楼主下载,tieba_image
+
 p=0
-tieba = input('贴吧名：')
-folder = os.getcwd()+'/'+tieba # xxx = os.getcwd() #获取当前绝对路径 # + 编写目录
+tieba = input('贴吧名（不带吧）：')
+folder = os.getcwd()+'/'+tieba # os.getcwd() #获取当前绝对路径 # + 编写目录
 test=os.path.exists(folder)
 if not test:
-        os.mkdir(folder)            #--------------------创建theme文件夹 
-else:
-        pass
-input1 = input('贴吧链接是否带https？输入y/n：')
-while True:
+        os.mkdir(folder)            #--------------------创建文件夹 
+
+input1 = input('贴吧链接是否带https？输入y/n（小写）： ')
+inputjpg = input('下载文章输入1，下载图片输入2：\n')
+
+#------------------------------最后一页检测：
+lastpage = requests.get('https://tieba.baidu.com/f?kw='+str(tieba)).text
+soup = BeautifulSoup(lastpage,'lxml')
+last_item =soup.find_all('a',attrs={'class':'last pagination-item'})
+lastpage = int(last_item[0]['href'].split('=')[-1])
+print(lastpage)
+print('总页数：%s'%(lastpage/50))
+
+
+while True:#获取帖子链接，循环翻页
     url = 'https://tieba.baidu.com/f?kw='+str(tieba)+'&ie=utf-8&pn='+str(p)
     r=requests.get(url).text
     p += 50
-    soup = BeautifulSoup(r,'lxml')
     urllist=soup.find_all('a',attrs={'class':'j_th_tit'})
     a = re.findall('href=\"/p/.*?\"',str(urllist))#正则后
+    print('当前页数：')
     print(p/50)
-    if len(a) == '0':
-        print('没有帖子了！')
-        input()
+    if p > lastpage:
+        print('没有帖子了！任务完成，共爬取了%s个帖子'%lastpage)
+        input('')
+        break
     if input1 == 'y':
         # print(urllist)
         # print('---------------------------------')
         for aa in a:
             aa = 'https://tieba.baidu.com'+aa[6:-1]#构造完整链接
-            贴吧只看楼主下载.run(aa,tieba)
+            if inputjpg == '1':
+                贴吧只看楼主下载.run(aa,tieba)
+            else:
+                tieba_image.run(aa,tieba)
     else:
         for aa in a:
             aa = 'http://tieba.baidu.com'+aa[6:-1]#构造完整链接
-            贴吧只看楼主下载.run(aa,tieba)
+            if inputjpg == '1':
+                贴吧只看楼主下载.run(aa,tieba)
+            else:
+                tieba_image.run(aa,tieba)
     
